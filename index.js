87752cefe63df26794e2abc2a308516e97acc7b6
AYTEKİN KAPLAN <112782378+aytekinkaplan@users.noreply.github.com>
2024-08-30 19:18:44 +0300

    Up

--- index.js
+++ index.js
@@ -3,8 +3,11 @@ const app = express();
 const path = require("path");
 const ejs = require("ejs");
 app.set("view engine", "ejs");
-app.set("views", path.join(__dirname, "views"));
+app.set("views", path.join(__dirname, "src/views"));
 app.get("/", (req, res) => {
   res.render("index");
+});
+app.get("/about", (req, res) => {
+  res.render("about");
 });
 app.listen(3000);
